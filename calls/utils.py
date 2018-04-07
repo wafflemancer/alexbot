9c274ccc04df57f26c7bb24d30112790ca12690c
GavinMegson <33714341+GavinMegson@users.noreply.github.com>
2018-04-07 15:01:02 -0700

    Update utils.py
    joke function created

--- calls/utils.py
+++ calls/utils.py
@@ -3,8 +3,10 @@ import calls.custom_dialogue as cd
 
 
 all_function = {'raw': raws.latest(),
-                'pad': raws.pad()}
+                'pad': raws.pad(),
+                'joke: get_joke()}
 
+ 
 
 def get_function(msg):
     if msg == 'help':
@@ -28,4 +30,28 @@ def get_dialogue(msg):
             else:
                 return None
     else:
+        return None
+
+                
+# Get random joke from custom dialogue
+# Note: due to legacy implementation,
+# number of jokes is hardcoded to
+# be randomized, as well as line #.                
+NUM_JOKES = 6
+                
+def get_joke(msg):
+    bank = cd.dialogue_bank()
+    msg = msg.strip()
+    num = randint(1,NUM_JOKES)
+    joke = str(num) + msg
+    if joke in bank.keys():
+        convo = bank[joke]
+            if convo[1] is None:
+                return convo[0]
+            else:
+                if convo[1](joke):
+                    return convo[0]
+                else:
+                    return None
+     else:
         return None
